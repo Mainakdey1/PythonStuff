f046354fae32c16dd5c7e29e4bc1fe269e9a739a
Mainak Dey <81181897+Mainakdey1@users.noreply.github.com>
2022-08-11 17:14:18 +0530

    Update valorant.py

--- valorant.py
+++ valorant.py
@@ -1,8 +1,13 @@
+#This code takes values for map aspects and predicts the site you might have the best chance of taking with the
+#least number of players lost
+#the fundamental functions are made such that they can account for most competitive games
+#with minimal change to code
+#To run this program, use it in a python compiler(python 2.7+ recommended)
 
-
+#function to calculate probability of success and generate stats.
 def decide(maps,lastmap,rnd,players,mapno,fake):
-    
-    def msearch(l,target,low,high):
+    #searches through the given list of maps.
+    def msearch(l,target,low,high):                    
         if low>high:
             return False
         else:
@@ -14,18 +19,27 @@ def decide(maps,lastmap,rnd,players,mapno,fake):
                     return msearch(l,target,low,mid-1)
                 else:
                     return msearch(l,target,mid+1,high)
-    index=msearch(maps,lastmap,0,len(maps)-1)
+    fconstant=(2/len(maps))*fake
+    index=msearch(maps,lastmap,0,len(maps)-1)   
+    #assigned a variable index to the index of the provided last map
     def ovsc(rnd):
+        #calculates strategy failure/success given the round number it is used in
         if rnd%fake:
             return rnd
         else:
             return 0
+        
+    #the following component calculates failure percentage by calculating a failure constant inversely proportional to the
+    #number of sites and proportional to the number of different strategies applied.
+    #with every repetition of strategy
+    #this is very generic but is still applicable where the map pool is large
+    #
     if index<(len(maps)-1)/2:
         print("You can enter site",maps[len(maps)-1],"with a loss of atmost",players//mapno,"players")
-        print("The overall chances of failure are",ovsc(rnd)*2,"%")
+        print("The overall chances of failure are",ovsc(rnd)*fconstant,"%")
     else:
         print("You can enter site",maps[0],"with a loss of atmost",players//mapno,"players")
-        print("The overall chances of failure are",ovsc(rnd)*2,"%")
+        print("The overall chances of failure are",ovsc(rnd)*fconstant,"%")
 fake=int(input("How many different strats do you intend to apply?: "))
 mapname=str(input("Enter the map name: "))
 roundno=int(input("Enter the roundno: "))
